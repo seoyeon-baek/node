2b1c55fc8aba68bc3522cc783572b7a32f150354
seoyeon-baek <w2032@e-mirim.hs.kr>
2022-03-18 13:59:01 +0900

    Add: function

---
+++ MoonBub/function.js
@@ -0,0 +1,13 @@
+function 문자출력함수(문자){
+    console.log(문자)
+}
+문자출력함수("내피시지킴이");
+
+printChar = 문자출력함수
+printChar('b');
+
+printChar = function(문자) {
+    console.log(문자);
+};
+
+printChar('청소를 열심히 합시다');
