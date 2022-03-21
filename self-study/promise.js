dd7453385f3bcf4b882081148719c50737a9531f
seoyeon-baek <w2032@e-mirim.hs.kr>
2022-03-21 13:56:00 +0900

    Add: promise 2

--- self-study/promise.js
+++ self-study/promise.js
@@ -1,9 +1,9 @@
-let myFirstPromise = new Promise((resolve, reject) => {
-    setTimeout( function(){
-        resolve('성공!')
-    }, 2500)
-})
-
-myFirstPromise.then((successMessage) => { //successMessage는 resolve에서 호출에 제공한 값
-    console.log("와!" + successMessage);
-})
+// let myFirstPromise = new Promise((resolve, reject) => {
+//     setTimeout( function(){
+//         resolve('성공!')
+//     }, 2500)
+// })
+//
+// myFirstPromise.then((successMessage) => { //successMessage는 resolve에서 호출에 제공한 값
+//     console.log("와!" + successMessage);
+// })
