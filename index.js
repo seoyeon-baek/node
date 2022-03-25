28a63922a0d63bde9e9769401979a9e266218b1b
seoyeon-baek <w2032@e-mirim.hs.kr>
2022-03-25 14:46:52 +0900

    Add: static server page

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const http = require('http');
+const fs = require("fs");
 
 const app = http.createServer(function (req, res){
+    let url = req.url;
+
+    if(url === '/')
+        url = '/index.html';
+    if(url === '/favicon.ico')
+        return res.writeHead(404);
+
     res.writeHead(200);
-    console.log(__dirname + req.url); //folder address
-    res.end("hello world");
+    res.end(fs.readFileSync(__dirname + url));
 });
 
-app.listen(3000); //3000 port start, port change
+app.listen(3333); //3000 port start, port change
