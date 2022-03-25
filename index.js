9767dbd4a52899feb0feeb0369f59b15b22d6084
seoyeon-baek <w2032@e-mirim.hs.kr>
2022-03-25 15:07:32 +0900

    Add: simple dynamic page

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 const http = require('http');
 const fs = require("fs");
+const url = require('url');
 
 const app = http.createServer(function (req, res){
-    let url = req.url;
-
-    if(url === '/')
-        url = '/index.html';
-    if(url === '/favicon.ico')
-        return res.writeHead(404);
-
-    res.writeHead(200);
-    res.end(fs.readFileSync(__dirname + url));
+    let _url = req.url;
+    let queryData = url.parse(_url, true).query;
+    res.end(queryData.id);
 });
 
 app.listen(3333); //3000 port start, port change
