0d5487ad57f95fef60a8109dd113501f786385eb
seoyeon-baek <w2032@e-mirim.hs.kr>
2022-04-11 14:33:43 +0900

    Add: 글생성 파일생성과 리다이렉션

--- index.js
+++ index.js
@@ -84,9 +84,10 @@ const app = http.createServer(function (request, response) {
             const post = qs.parse(body);
             const title = post.title;
             const description = post.description;
+            fs.writeFile(`data/${title}`, description, 'utf-8', function(err){})
+            response.writeHead(302, {Location: `/?id=${title}`});
+            response.end();
         })
-        response.writeHead(200);
-        response.end("success");
 
     }else {
         response.writeHead(404)
